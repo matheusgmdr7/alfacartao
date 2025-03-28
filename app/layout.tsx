571e7b75f0a33d55a4507e322a7317d7b653bf6b
matheusgmdr7 <matheusgmdr7@gmail.com>
2025-03-28 01:34:46 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,9 +3,9 @@ import type { Metadata } from "next"
 import "./globals.css"
 
 export const metadata: Metadata = {
-  title: "v0 App",
-  description: "Created with v0",
-  generator: "v0.dev",
+  title: "AlfaSeguros",
+  description: "AlfaSeguros",
+  generator: "AlfaSeguros",
 }
 
 export default function RootLayout({
